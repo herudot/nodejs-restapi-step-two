6dcbc43f748cbe2580345d1a3b547bbfc8df3826
Heru Oktafian <heruoktafian.dot@gmail.com>
2019-06-24 00:49:44 +0700

    Adding middleware to correct value that have to insert into mongodb

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.use(bodyParser.json());
 // Implementation of express router which created before
 app.use('/api',routers);
 
+// Implementation of Error using middleware
+app.use(function(err, req, res, next){
+    res.status(422).send({err: err.message});
+});
+
 app.listen( process.env.port || 5000, function(req, res){
   console.log('Express server started !');
 });
