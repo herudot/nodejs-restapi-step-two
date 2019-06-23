1e1697b779c8138015e855fc0680a00a83869e5c
Heru Oktafian <heruoktafian.dot@gmail.com>
2019-06-24 00:28:03 +0700

    Create Models & Schema of Students

---
+++ models/students.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const StudentsSchema = new Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  age:{
+    type: Number,
+    required: true
+  },
+  gender:{
+    type: String,
+    required: true
+  },
+  address:{
+    type: String,
+    required: false
+  }
+});
+
+const Students = mongoose.model('students', StudentsSchema);
+
+module.exports = Students;
