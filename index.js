b8e64cd97a07581abda6832a9e78834ab8bf349d
Heru Oktafian <heruoktafian.dot@gmail.com>
2019-06-24 00:35:03 +0700

    Create Connection into index.js

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routers = require('./routers');
 const app = express();
+const mongoose = require('mongoose');
 
+// DB Connection
+mongoose.connect('mongodb://localhost:27017/sinau-nodejs', {useNewUrlParser: true, useFindAndModify: false});
+mongoose.Promise = global.Promise;
+
+// Implementation of body-parser
 app.use(bodyParser.json());
+
+// Implementation of express router which created before
 app.use('/api',routers);
 
 app.listen( process.env.port || 5000, function(req, res){
